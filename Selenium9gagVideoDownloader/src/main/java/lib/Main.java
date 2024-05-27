7be65260dfac88b517d32766b8c78711a87114c0
Djabeu <rafal.chojnowski1988@gmail.com>
2024-05-27 21:48:50 +0200

    add scrolling and clicking cookies button to 9gag scraper selenium solution

--- Selenium9gagVideoDownloader/src/main/java/lib/Main.java
+++ Selenium9gagVideoDownloader/src/main/java/lib/Main.java
@@ -1,12 +1,16 @@
 package lib;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.*;
 import java.net.URL;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,7 +24,7 @@ public class Main {
         try {
             script(webDriver);
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+            exception.printStackTrace();
         }
 
         webDriver.quit();
@@ -28,24 +32,49 @@ public class Main {
 
     private static void script(WebDriver webDriver) throws IOException {
         webDriver.get("https://9gag.com/u/srmaryhermanngo/posts");
-        List<WebElement> videos = webDriver.findElements(By.xpath(".//video"));
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.parse("PT10S"));
+        WebElement cookiesButton = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("onetrust-accept-btn-handler")));
+        cookiesButton.click();
+
+        getSourcesAndTitles(webDriver);
+
+//        downloadFiles(sourcesAndTitles);
+    }
+
+    private static void getSourcesAndTitles(WebDriver webDriver) {
         Map<String, String> sourcesAndTitles = new HashMap<>();
+
+        findSourcesAndTitles(sourcesAndTitles, webDriver);
+
+    }
+
+    private static void findSourcesAndTitles(Map<String, String> sourcesAndTitles, WebDriver webDriver) {
+        JavascriptExecutor js = (JavascriptExecutor) webDriver;
+        List<WebElement> videos = webDriver.findElements(By.xpath(".//video[@preload='auto']"));
 
         for (WebElement video : videos) {
             WebElement headerElement = video.findElement(By.xpath("./../../../../../header/a/h2"));
             String title = fixTitle(headerElement.getText());
+            if (!sourcesAndTitles.containsValue(title)) {
+                js.executeScript("arguments[0].scrollIntoView(true);", headerElement);
+            }
 
             List<WebElement> sourcesElements = video.findElements(By.xpath("./source"));
 
             for (WebElement sourceElement : sourcesElements) {
                 String sourceSrc = sourceElement.getAttribute("src");
                 if (sourceSrc.endsWith(".mp4")) {
-                    sourcesAndTitles.put(sourceElement.getAttribute("src"), title);
+                    if(!sourcesAndTitles.containsKey(sourceElement.getAttribute("src"))){
+                        sourcesAndTitles.put(sourceElement.getAttribute("src"), title);
+                    }
+                    if (sourcesAndTitles.size() == videos.size()) {
+                        js.executeScript("window.scrollBy(0,1000)", "");
+                        findSourcesAndTitles(sourcesAndTitles, webDriver);
+                    }
                 }
             }
         }
-
-//        downloadFiles(sourcesAndTitles);
     }
 
     private static String fixTitle(String title) {
